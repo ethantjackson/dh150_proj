cfc5cf85215672a8e79f59d2a66c52ea7843e0db
Ethan Jackson <ethantjackson44@gmail.com>
2024-02-04 22:43:25 -0800

    Add introduction text page

--- src/App.js
+++ src/App.js
@@ -16,12 +16,17 @@ function App() {
     if (!parallaxRef.current) return;
     const scrollRatio = parallaxRef.current.current / parallaxRef.current.space;
     setScrollRatio(scrollRatio);
-    const newVolume = Math.min(1, (scrollRatio * 2) ** 2);
+    const newVolume = Math.min(
+      1,
+      (scrollRatio * 2) ** 2,
+      ((scrollRatio - 2) * 2) ** 2
+    );
     if (introVideo.current) introVideo.current.volume = newVolume;
     if (newVolume > 0 && introVideo.current.paused) {
       introVideo.current.currentTime = 92;
       introVideo.current.play();
     }
+    if (newVolume === 0) introVideo.current.pause();
   };
 
   useEffect(() => {
@@ -83,6 +88,7 @@ function App() {
                   flexDirection: 'column',
                   justifyContent: 'center',
                   height: '100%',
+                  opacity: `${1 - scrollRatio * 2}`,
                 }}
               >
                 <Typography variant='h1' color='textPrimary'>
@@ -91,10 +97,11 @@ function App() {
               </Box>
             </ParallaxLayer>
             <ParallaxLayer
-              sticky={{ start: 0, end: 1 }}
+              sticky={{ start: 0, end: 2 }}
               style={{
                 opacity: `${scrollRatio * 2}`,
                 backgroundColor: 'black',
+                zIndex: '-1',
               }}
             >
               <Box sx={{ width: '100vw', height: '100vh' }}>
@@ -103,6 +110,7 @@ function App() {
                   id='introVideo'
                   width='100%'
                   height='100%'
+                  loop
                 >
                   <source src='/intro.mp4' type='video/mp4' />
                   Your browser does not support the video tag.
@@ -110,9 +118,67 @@ function App() {
               </Box>
             </ParallaxLayer>
             <ParallaxLayer offset={2}>
-              <Typography variant='h1' color='textPrimary'>
-                Test
-              </Typography>
+              <Box
+                component='img'
+                src='https://assets3.thrillist.com/v1/image/1679673/1200x630'
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  left: '0',
+                  top: '0',
+                }}
+              />
+              <Box
+                sx={{
+                  backgroundColor: 'rgba(0,0,0,0.8)',
+                  position: 'absolute',
+                  top: '0',
+                  left: `${-70 * (scrollRatio - 1) + 70 * 2}vw`,
+                  width: '30vw',
+                  height: '100vh',
+                }}
+              >
+                <Box
+                  sx={{
+                    marginLeft: '40px',
+                    width: 'calc(100% - 80px)',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Typography variant='h2' color='textPrimary' mb={2}>
+                    The Home of Jazz
+                  </Typography>
+                  <Typography
+                    variant='body1'
+                    color='textPrimary'
+                    sx={{ textAlign: 'justify' }}
+                    mb={2}
+                  >
+                    The Vanguard Village, located in Greenwich Village, New
+                    York, has hosted nearly every jazz great since its opening
+                    in 1935. The majority of influential jazz musicians, from
+                    Bill Evans to John Coltrane, all have live recordings at the
+                    Village Vanguard among their discographies.
+                  </Typography>
+                  <Typography
+                    variant='body1'
+                    color='textPrimary'
+                    sx={{ textAlign: 'justify' }}
+                  >
+                    In my final DH150 project, I aim to explore the history of
+                    the Village Vanguard and tell the story of how this club has
+                    shaped the state of jazz. Jazz is quintesentially American
+                    music, and is intertwined with America's history of race and
+                    culture. It has been a force of social change and a platform
+                    for self-expression, and it would not be what it is today
+                    without the Village Vanguard at its heart.
+                  </Typography>
+                </Box>
+              </Box>
             </ParallaxLayer>
           </Parallax>
         )}
