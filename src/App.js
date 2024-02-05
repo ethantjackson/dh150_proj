b65388d94cc26d836b7102b4ea7637d91396edc4
Ethan Jackson <ethantjackson44@gmail.com>
2024-02-04 20:07:14 -0800

    Add Freddie Hubbard intro video

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import { useEffect, useRef, useState } from 'react';
-import YouTube from 'react-youtube';
-import {
-  createTheme,
-  Container,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { createTheme, ThemeProvider, Typography, Box } from '@mui/material';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
   const parallaxRef = useRef();
-  const youtubePlayer = useRef();
+  const introVideoRef = useCallback((node) => {
+    console.log(node);
+    introVideo.current = node;
+  }, []);
+  const introVideo = useRef();
   const [scrollRatio, setScrollRatio] = useState(0);
+  const [readyClicked, setReadyClicked] = useState(false);
 
   const handleScroll = () => {
     if (!parallaxRef.current) return;
     const scrollRatio = parallaxRef.current.current / parallaxRef.current.space;
     setScrollRatio(scrollRatio);
-    if (!youtubePlayer.current) return;
-    youtubePlayer.current.setVolume((scrollRatio * 40) ** 2);
+    const newVolume = Math.min(1, (scrollRatio * 2) ** 2);
+    if (introVideo.current) introVideo.current.volume = newVolume;
+    if (newVolume > 0 && introVideo.current.paused) {
+      introVideo.current.currentTime = 92;
+      introVideo.current.play();
+    }
   };
 
   useEffect(() => {
+    if (!readyClicked) return;
     const container = document.querySelector('.parallax');
     container.addEventListener('scroll', handleScroll);
     return () => {
       container.removeEventListener('scroll', handleScroll);
     };
-  }, []);
-
-  const introVidId = 'zBs9gZQX7lQ';
-  const introVidOpts = {
-    height: '100%',
-    width: '100%',
-    playerVars: {
-      autoplay: 1,
-      loop: 1,
-      controls: 0,
-      disablekb: 1,
-      origin: window.location.origin,
-    },
-  };
+  }, [readyClicked]);
 
   const themeOptions = {
     palette: {
@@ -51,44 +42,81 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container
+      <Box
         sx={{
-          height: '100%',
-          width: '100%',
+          height: '100vh',
+          width: '100vw',
           textAlign: 'center',
+          backgroundColor: 'black',
         }}
       >
-        <Parallax
-          className='parallax'
-          ref={parallaxRef}
-          pages={2}
-          style={{
-            left: '0',
-            top: '0',
-            backgroundColor: 'black',
-            opacity: `${1 - scrollRatio * 3}`,
-          }}
-        >
-          <ParallaxLayer offset={0.3} speed={-0.5}>
-            <Typography variant='h1' color='textPrimary'>
-              Jazz
+        {!readyClicked ? (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              height: '100%',
+              cursor: 'pointer',
+            }}
+            onClick={() => setReadyClicked(true)}
+          >
+            <Typography color='textPrimary' variant='h6'>
+              Click To Begin
             </Typography>
-          </ParallaxLayer>
-        </Parallax>
-      </Container>
-      <YouTube
-        videoId={introVidId}
-        opts={introVidOpts}
-        style={{ height: '100vh', width: '100vw' }}
-        onReady={(event) => {
-          event.target.setVolume(0);
-          event.target.playVideo();
-          youtubePlayer.current = event.target;
-        }}
-        onEnd={(event) => {
-          event.target.playVideo();
-        }}
-      />
+          </Box>
+        ) : (
+          <Parallax
+            className='parallax'
+            ref={parallaxRef}
+            pages={3}
+            style={{
+              left: '0',
+              top: '0',
+              backgroundColor: 'black',
+            }}
+          >
+            <ParallaxLayer offset={0} speed={-0.5}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  height: '100%',
+                }}
+              >
+                <Typography variant='h1' color='textPrimary'>
+                  The Village Vanguard
+                </Typography>
+              </Box>
+            </ParallaxLayer>
+            <ParallaxLayer
+              sticky={{ start: 0, end: 1 }}
+              style={{
+                opacity: `${scrollRatio * 2}`,
+                backgroundColor: 'black',
+              }}
+            >
+              <Box sx={{ width: '100vw', height: '100vh' }}>
+                <video
+                  ref={introVideoRef}
+                  id='introVideo'
+                  width='100%'
+                  height='100%'
+                >
+                  <source src='/intro.mp4' type='video/mp4' />
+                  Your browser does not support the video tag.
+                </video>
+              </Box>
+            </ParallaxLayer>
+            <ParallaxLayer offset={2}>
+              <Typography variant='h1' color='textPrimary'>
+                Test
+              </Typography>
+            </ParallaxLayer>
+          </Parallax>
+        )}
+      </Box>
     </ThemeProvider>
   );
 }
